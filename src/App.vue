afdcaadb834d67ecca76aeb7478e70ebadeadda0
闫小婧 <632884698@qq.com>
2019-03-21 18:18:01 +0800

    t1

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
 		<background></background>
 		<desktopApp></desktopApp>
+		<menuBar></menuBar>
   </div>
 </template>
 
 <script>
 import background from './components/background.vue'
 import desktopApp from './components/desktopApp.vue'
+import menuBar from './components/menuBar.vue'
+
 import windowData from './static/js/demo-data.js'
 export default {
   name: 'app',
   components: {
 		background,
-		desktopApp
+		desktopApp,
+		menuBar
   },
 	created(){
 		this.$store.commit("windowData/setdata",windowData)
